13ef2cfb479ba21b7a898be9a0da2fe78634bc6c
Renato Bento <renatobento@Renatos-MacBook.local>
2019-06-20 18:04:59 -0300

    commit inicial

---
+++ desafio-wooza/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default class App extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <div className="card">
+                    <img src="" /> 
+                    <div>
+                        <span>Nome Funcionário</span>
+                        <h1>Renato Bento</h1>
+                        <hr/>
+                        <span>Cargo</span>
+                        <h1>Desenvolvedor Front-end</h1>
+                    </div>
+                </div>
+            </div>
+            
+            
+            )
+    }
+
+}
